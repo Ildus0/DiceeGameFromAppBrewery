5540d149670025e6568bd481f3f41c9d13fd2e2b
ILDUS <yakupovildus86@gmail.com>
2022-04-26 16:17:25 +0400

    initial commit

--- script.js
+++ script.js
@@ -15,22 +15,22 @@ console.log(firstDice);
 
 switch (firstDice) {
     case 1:
-        img1.setAttribute("src", "images/dice1.png");
+        img1.setAttribute("src", "dice1.png");
         break;
     case 2:
-        img1.setAttribute("src", "images/dice2.png");
+        img1.setAttribute("src", "dice2.png");
         break;
     case 3:
-        img1.setAttribute("src", "images/dice3.png");
+        img1.setAttribute("src", "dice3.png");
         break;
     case 4:
-        img1.setAttribute("src", "images/dice4.png");
+        img1.setAttribute("src", "dice4.png");
         break;
     case 5:
-        img1.setAttribute("src", "images/dice5.png");
+        img1.setAttribute("src", "dice5.png");
         break;
     case 6:
-        img1.setAttribute("src", "images/dice6.png");
+        img1.setAttribute("src", "dice6.png");
         break;
 }
 click2();
@@ -49,22 +49,22 @@ function click2(){
 console.log(secondDice);
     switch (secondDice) {
         case 1:
-            img2.setAttribute("src", "images/dice1.png");
+            img2.setAttribute("src", "dice1.png");
             break;
         case 2:
-            img2.setAttribute("src", "images/dice2.png");
+            img2.setAttribute("src", "dice2.png");
             break;
         case 3:
-            img2.setAttribute("src", "images/dice3.png");
+            img2.setAttribute("src", "dice3.png");
             break;
         case 4:
-            img2.setAttribute("src", "images/dice4.png");
+            img2.setAttribute("src", "dice4.png");
             break;
         case 5:
-            img2.setAttribute("src", "images/dice5.png");
+            img2.setAttribute("src", "dice5.png");
             break;
         case 6:
-            img2.setAttribute("src", "images/dice6.png");
+            img2.setAttribute("src", "dice6.png");
             break;
     }
 }
